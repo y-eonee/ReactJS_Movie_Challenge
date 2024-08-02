bbb9e947973e3bb829db0ecb492b1888ac1df76b
yeonee <155801470+y-eonee@users.noreply.github.com>
2024-08-03 03:52:40 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     <Header />
     <Routes>
       <Route path="/movie/:id" element = {<Detail/>} />
-      <Route path="/" element={<Home/>} />
+      <Route path={`${process.env.PUBLIC_URL}/`} element={<Home/>} />
     </Routes>
   </Router>;
   

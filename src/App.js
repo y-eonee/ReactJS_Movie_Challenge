4b6d2ebce8e1cb58d0e374186cb5ccff4c766512
yeonee <155801470+y-eonee@users.noreply.github.com>
2024-08-03 12:50:41 +0900

    router 수정

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     <>
       <Header />
       <Routes>
-        <Route path={`${process.env.PUBLIC_URL}/movie/:id`} element={<Detail />} />
+        <Route path={`${process.env.PUBLIC_URL} + /movie/:id`} element={<Detail />} />
         <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
       </Routes>
     </>
